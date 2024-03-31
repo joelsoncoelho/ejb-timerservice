c48ec19c7423e7df7c4358592d40a82a5628689f
Joelson Coelho <joelson.save@gmail.com>
2024-03-31 16:17:37 -0300

    first commit

---
+++ ejbModule/com/ejb/timerservice/Agendador.java
@@ -0,0 +1,15 @@
+package com.ejb.timerservice;
+
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+@Singleton
+@Startup
+public class Agendador {
+
+    @Schedule(hour="*", minute="*", second="*/10", persistent=false)    
+    void agendado() {
+        System.out.println("verificando serviço externo periodicamente");
+    }
+}
